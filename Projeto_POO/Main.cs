61a2c934637d92a7f840cdf5cb070094c688163a
Lívia Rachel <83011310+Livia-Rachell@users.noreply.github.com>
2022-01-02 19:22:56 +0000

    .

--- Projeto_POO/Main.cs
+++ Projeto_POO/Main.cs
@@ -2,10 +2,6 @@ using System;
 
 class Program{
   public static void Main(){
-<<<<<<< HEAD
     Console.WriteLine("Lívia é Maravilhosa!");
-=======
-    Console.WriteLine("Alcides é maravilhosamente lindo e perfeito!");
->>>>>>> origin/main
   }
 }
